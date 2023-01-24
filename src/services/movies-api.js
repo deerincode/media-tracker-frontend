f1a12cecef0c2500864f9406494ccd746beeeebd
Vogue Doesong <nboney@email.sc.edu>
2023-01-24 11:38:06 -0500

    Added all endpoints to movie api

---
+++ src/services/movies-api.js
@@ -0,0 +1,38 @@
+// Movie endpoints
+import axios from "axios";
+const baseURL = 'https://localhost:3001/movies'
+
+// Show all
+export const getMovies = () => {
+    const URL = baseURL
+    const response = axios.get(URL)
+    return response
+}
+
+// Show one entry
+export const getMovie = (id) => {
+    const URL = `${baseURL}/${id}`
+    const response = axios.get(URL)
+    return response
+}
+
+// Edit Movie entry
+export const editMovie = (id, updatedMovie) => {
+    const URL = `${baseURL}/${id}`
+    const response = axios.put(URL, updatedMovie)
+    return response
+}
+
+// Create Movie entry
+export const createMovie = (movie) => {
+    const URL = baseURL
+    const response = axios.post(URL, movie)
+    return response
+}
+
+// Delete Movie entry
+export const deleteMovie = (id) => {
+    const URL = `${baseURL}/${id}`
+    const response = axios.delete(URL)
+    return response
+}
